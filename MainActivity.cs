1181954d74b13756701375b9376e3dd4a868a35f
wqw792559263 <qq792559263@gmail.com>
2017-10-26 04:53:36 +0800

    Add files via upload

---
+++ MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace App4
+{
+    [Activity(Label = "App4", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+           
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            Button showPopupMenu = FindViewById<Button>(Resource.Id.popupButton);
+            showPopupMenu.Click += delegate
+            {
+                //Toast.MakeText(this, "hello", ToastLength.Long).Show();
+                PopupMenu menu = new PopupMenu(this.showPopupMenu);
+                menu.Inflate(Resource.Menu.popup_menu);
+                menu.MenuItemClick += (s1, e1) =>
+                {
+                    Toast.MakeText(this, e1.Item.TitleFormatted.ToString(), ToastLength.Long).Show();
+                };
+                menu.Show();  
+            };
+
+
+        }
+    }
+}
+
